c6f3317d7ba9892c141b3c1d3e73bded0b67d6a5
Nathalie Brochstein <nathalie@karibu.ai>
2024-08-17 19:46:22 -0700

    Build the Color Mode Switch

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ return (
     <NavBar/>
     </GridItem>
 <Show above="lg">
-<GridItem area='aside' bg='gold'>Aside</GridItem>
+<GridItem area='aside'>Aside</GridItem>
 </Show>
-<GridItem area='main' bg='dodgerblue'>Main</GridItem>
+<GridItem area='main' >Main</GridItem>
 </Grid>
 )
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,13 @@
-import { HStack, Image, Text } from '@chakra-ui/react'
+import { HStack, Image } from '@chakra-ui/react'
 import logo from '../assets/Logo_Karibu.webp';
+import ColorModeSwitch from './ColorModeSwitch';
 
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent='space-between' padding='10px'>
         <Image src={logo} boxSize={'60px'}/>
-       <Text>Nav Bar</Text>
+       <ColorModeSwitch />
     </HStack>
   )
 }
