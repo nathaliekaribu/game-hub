a14ed6735d2ab9e31817224ad1e6dbca205f850b
Nathalie Brochstein <nathalie@karibu.ai>
2024-08-18 14:09:45 -0700

    Remove Duplicated Styles (Skeleton) Still Not Rendering

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,10 +1,10 @@
-import { Card, CardBody, Skeleton, Skeletoneton, SkeletonText } from '@chakra-ui/react'
+import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react'
 
 
 const GameCardSkeleton = () => {
   return (
    
-    <Card width='200px' height='300px' borderRadius={10} overflow='hidden' >
+    <Card>
         <Skeleton height='200px'/>
         <CardBody>
             <SkeletonText/>
